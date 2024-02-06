13dbb23be3ed02035230242447637cbc6b6f0b55
Jukka <savela.j@gmail.com>
2024-02-06 17:05:03 +0200

    Added tours not interested button

--- pair-programming-1/src/components/Tours.js
+++ pair-programming-1/src/components/Tours.js
@@ -1,32 +1,31 @@
-import { tours } from "../data";
-import Tour from "./Tour";
-import Title from "./Title";
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { tours } from '../data';
+import Tour from './Tour';
+import Title from './Title';
 
 function Tours() {
-	const [toursData, setToursData] = useState(tours);
-  
-	const removeTour = (id) => {
-	  const updatedTours = toursData.filter((tour) => tour.id !== id);
-	  setToursData(updatedTours);
-	};
-  
-	return (
-	  <div>
-		<section className="section" id="tours">
-		  <Title title="featured" span="tours" />
-		  <div className="section-center featured-center">
-			{toursData.map((tour) => (
-			  <Tour key={tour.id} {...tour}>
-				<button onClick={() => removeTour(tour.id)}>
-				  Not Interested
-				</button>
-			  </Tour>
-			))}
-		  </div>
-		</section>
-	  </div>
-	);
-  }
-  
-  export default Tours;
+  const [toursData, setToursData] = useState(tours);
+
+  const removeTour = (id) => {
+    const updatedTours = toursData.filter((tour) => tour.id !== id);
+    setToursData(updatedTours);
+  };
+
+  return (
+    <div>
+      <section className="section" id="tours">
+        <Title title="featured" span="tours" />
+        <div className="section-center featured-center">
+          {toursData.map((tour) => (
+            <div key={tour.id}>
+              <Tour {...tour} />
+              <button onClick={() => removeTour(tour.id)}>Not Interested</button>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Tours;
